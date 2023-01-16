77df7a52bc5b56caf6a4e9d949840ae5a24b2d70
Marconi Soares <marconi.developer@outlook.com.br>
2023-01-16 14:25:15 -0300

    fix cadastro_e_login

--- flashcards/cadastro_e_login/serializers.py
+++ flashcards/cadastro_e_login/serializers.py
@@ -6,6 +6,11 @@ class UserCreateSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'username':{'required':True}
+        }
+
 
     def create(self, validated_data):
         return self.Meta.model.objects.create_user(**validated_data)
