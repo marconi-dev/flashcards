3678279aca77a7ef4f3eaf62a80fe91a7c52a21a
Marconi Soares <marconi.developer@outlook.com.br>
2023-02-09 10:12:01 -0300

    refatorando estudos pt 2

--- flashcards/estudos/permissions.py
+++ flashcards/estudos/permissions.py
@@ -2,17 +2,21 @@ from rest_framework.permissions import BasePermission
 
 from baralhos.models.models import Carta
 
+
 class TerminouDeRevisar(BasePermission):
+    """
+    O usuário só pode ver uma NOVA carta depois de terminar 
+    a revisão das cartas que já foram vistas.
+    """
     def has_permission(self, request, view):
         if request.method != 'PUT': return True
         
         baralho_pk = view.kwargs.get('baralho_pk')
         pk = view.kwargs.get('pk')
 
-        carta_foi_vista = Carta.objects.values("vista").get(pk=pk)['vista']
+        carta_nao_vista = not Carta.objects.values("vista").get(pk=pk)['vista']
         cartas_para_revisar = Carta.objects.para_revisar(baralho_pk).exists()
 
-        if carta_foi_vista: return True
-        if cartas_para_revisar: return False
-        else: return True
-        
+        if carta_nao_vista and cartas_para_revisar: 
+            return False
+        return True
--- flashcards/estudos/serializers.py
+++ flashcards/estudos/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+
 class EstudosSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     frente_imagem = serializers.ImageField(source='frente.imagem')
