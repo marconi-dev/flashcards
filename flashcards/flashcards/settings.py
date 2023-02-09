2c395a1fae0a3ca8c6b1b06df913e4a90e167c02
Marconi Soares <marconi.developer@outlook.com.br>
2023-02-09 18:54:55 -0300

    add S3

--- flashcards/flashcards/settings.py
+++ flashcards/flashcards/settings.py
@@ -198,4 +198,14 @@ if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True 
 else:
     CORS_ALLOWED_ORIGINS = [os.getenv("CORS_ALLOWED_ORIGINS")]
- 
+
+
+#AWS S3
+if DEBUG == False:
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
