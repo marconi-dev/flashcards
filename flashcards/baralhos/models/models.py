dd2978b49513ecfeb23352bbf885a4622913d496
Marconi Soares <marconi.developer@outlook.com.br>
2023-03-17 17:12:26 -0300

    baralho/models.py

--- flashcards/baralhos/models/models.py
+++ flashcards/baralhos/models/models.py
@@ -5,17 +5,21 @@ from baralhos.models import managers
 
 
 # Create your models here.
+HOJE = date.today()
+AMANHA = HOJE + timedelta(days=1)
+
+
 class Tag(models.Model):
     nome = models.CharField(max_length=64, unique=True)
 
     def __str__(self):
         return self.nome
 
-HOJE = date.today()
-AMANHA = HOJE + timedelta(days=1)
+
 class Baralho(models.Model):
     usuario = models.ForeignKey(
-        "cadastro_e_login.User", on_delete=models.CASCADE,
+        "cadastro_e_login.User", 
+        on_delete=models.CASCADE,
         related_name='baralhos'
     )
     nome = models.CharField(max_length=64)
@@ -24,22 +28,18 @@ class Baralho(models.Model):
     tags = models.ManyToManyField(Tag)
     objects = managers.BaralhoManager()
 
-
     def __str__(self):
         return self.nome
 
 
 class Carta(models.Model):
     baralho = models.ForeignKey(
-        Baralho, on_delete=models.CASCADE, 
+        Baralho, 
+        on_delete=models.CASCADE, 
         related_name='cartas'
     )
-    frente = models.OneToOneField(
-        'baralhos.Frente', on_delete=models.CASCADE
-    )
-    verso = models.OneToOneField(
-        'baralhos.Verso', on_delete=models.CASCADE
-    )
+    frente = models.OneToOneField('baralhos.Frente', on_delete=models.CASCADE)
+    verso = models.OneToOneField('baralhos.Verso', on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag)
     proxima_revisao = models.DateField()
     vista = models.BooleanField(default=False)
@@ -75,17 +75,17 @@ class Carta(models.Model):
         self._marcar_revisao()
         self.save()
 
+
 class Frente(models.Model):
-    imagem = models.ImageField(
-        upload_to='frente', blank=True, null=True
-    )
-    texto = models.CharField(
-        max_length=255, null=True, blank=True
-    )
+    imagem = models.ImageField(upload_to='frente', blank=True, null=True)
+    texto = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self) -> str:
-        if self.texto: return self.texto
-        return f'Carta de Imagem'
+        if self.texto: 
+            return self.texto
+        else: 
+            return f'Carta de Imagem'
+
 
 class Verso(models.Model):
     texto = models.CharField(max_length=255)
